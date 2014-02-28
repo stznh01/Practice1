d0d4349b4af29ae39b10b4d7af64655d53343025
Zixuan Huang <stznh01@moravian.edu>
2014-02-27 23:04:40 -0500

    Initial Commit

---
+++ Practice1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Practice1
+//
+//  Created by Hansen on 2/27/14.
+//  Copyright (c) 2014 Zixuan Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
